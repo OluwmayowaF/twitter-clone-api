d1694f2c422ef673e0f5594586edd39980ac5700
oluwamayowaf <e_fadairo@yahoo.com>
2020-03-31 19:21:18 +0100

    Chore(User Model) Created the user model for the database

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+// Create the user schema 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstname: {
+    type: String,
+  },
+  lastname: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const user = mongoose.model('users', userSchema);
+
+module.exports = user;
